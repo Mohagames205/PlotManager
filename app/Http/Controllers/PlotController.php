07b438638a1e3a56817dcf1fc7ba9a294434c584
Mohamed <40402787+Mohagames205@users.noreply.github.com>
2020-03-22 16:44:02 +0100

    Initial commit

---
+++ app/Http/Controllers/PlotController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Plot;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PlotController extends Controller
+{
+
+
+
+
+    public function index(){
+        $plots = DB::table("plots")->orderBy("plot_id")->get();
+
+        return view("welcome", ["plots" => $plots]);
+    }
+
+
+    public function get(Plot $plot){
+
+    }
+
+
+    public function update(int $id)
+    {
+        $request = request();
+        $plot_id = $request->get("plot_id");
+        $plot_name = $request->get("plot_name");
+        $plot_owner = $request->get("plot_owner");
+        $plot_location = $request->get("plot_location");
+        $plot_members = $request->get("plot_members");
+        $plot_permissions = $request->get("plot_permissions");
+        $max_members = $request->get("plot_max_members");
+        $price = $request->get("plot_price");
+        $size = $request->get("plot_size");
+
+
+        Plot::where("plot_id", $id)->update(["plot_size" => $size, "plot_name" => $plot_name, "plot_owner" => $plot_owner, "plot_location" => $plot_location, "plot_members" => $plot_members, "plot_permissions" => $plot_permissions, "max_members" => $max_members, "plot_price" => $price]);
+
+        return response("Plot is geupdate!");
+
+    }
+
+
+
+    public function delete(int $id){
+
+        Plot::where("plot_id", $id)->delete();
+
+        return response("Plot is gedelete.");
+    }
+
+
+    public function create(Request $request){
+
+        if(!Plot::where("plot_id", $request->get("plot_id"))->exists()) {
+            $plot = new Plot();
+            $plot->plot_id = $request->get("plot_id");
+            $plot->plot_name = $request->get("plot_name");
+            $plot->plot_owner = $request->get("plot_owner");
+            $plot->plot_location = $request->get("plot_location");
+            $plot->plot_members = $request->get("plot_members");
+            $plot->plot_permissions = $request->get("plot_permissions");
+            $plot->max_members = $request->get("plot_max_members");
+            $plot->plot_price = $request->get("plot_price");
+            $plot->plot_size = $request->get("plot_size");
+
+            $plot->save();
+
+            return response("Plot is opgeslagen.");
+        }
+        else{
+            return response("Plot bestaat al!");
+        }
+    }
+
+
+
+
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+
+
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
+    <link rel="stylesheet" href="style/welcome.css">
+
+    <!-- Bootstrap -->
+
+    <title>TDB - Plots</title>
+</head>
+<body class="container mt-3">
+<div id="content">
+    <table id="plot_table" class="table table-striped">
+        <thead class="thead-dark">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Owner</th>
+            <th>Members</th>
+            <th>Oppervlakte</th>
+            <th>Prijs</th>
+
+        </tr>
+        </thead>
+        <tbody id="tablecontent">
+
+        @foreach($plots as $plot)
+            <tr>
+                <td>{{ $plot->plot_id }}</td>
+                <td> {{ $plot->plot_name }}</td>
+                <td> {{ $plot->plot_owner ? $plot->plot_owner : "Geen eigenaar" }} </td>
+                <td> {{ join(", ", json_decode($plot->plot_members)) ?  join(", ", json_decode($plot->plot_members)) : "Geen leden" }}</td>
+                <td> {{ json_decode($plot->plot_size)[0] }} x {{ json_decode($plot->plot_size)[1] }}</td>
+                <td> {{$plot->plot_price ? $plot->plot_price : "Niet te koop" }} </td>
+            </tr>
+
+        @endforeach
+
+        </tbody>
+
+    </table>
+</div>
+
+<script>
+    setInterval(function(){
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+
+                table.clear();
+                plots.forEach((plot)=>{
+                    table.row.add([
+                        '<td>{{ $plot->plot_id }}</td>'
+                    ]);
+                });
+            }
+        };
+        xhttp.open("GET", "/ajax", true);
+        xhttp.send();
+    },5000);
+
+</script>
+
+
+</body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script>
+    $(document).ready( function () {
+        var table = $('#plot_table').DataTable();
+    } );
+
+
+</script>
+
+</html>
